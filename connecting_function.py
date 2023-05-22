ad29b9d4d7f476c6334ecf5bffa4c3f34a01f09c
alexandercantrell <alexanderjoelcantrell@gmail.com>
2023-05-21 21:49:38 -0500

    testing connecting functions

--- connecting_function.py
+++ connecting_function.py
@@ -1,6 +1,40 @@
 import torch
 import torch.nn as nn
 
+def add_cnf(x,y):
+    return x+y
+
+def and_cnf(x,y):
+    return x*y
+
+def where_and_cnf(x,y):
+    z = x+y
+    return torch.where(z>1.0,1.0,0.0)
+
+def iand_cnf(x,y):
+    return x * (1. - y) 
+
+def where_iand_cnf(x,y):
+    z = x + 1. - y
+    return torch.where(z>1.0,1.0,0.0)
+
+def or_cnf(x,y):
+    return x+y-(x*y)
+
+def where_or_cnf(x,y):
+    z = x+y
+    return torch.where(z>0.0,1.0,z)
+
+def xor_cnf(x,y):
+    return x+y-(2*x*y)
+
+def modulus_xor_cnf(x,y):
+    return (x+y)%2
+
+def where_xor_cnf(x,y):
+    z = x+y
+    return torch.where(z>1.0,0.0,z)
+
 class ConnectingFunction(nn.Module):
     def __init__(self,cnf):
         super(ConnectingFunction,self).__init__()
@@ -13,8 +47,11 @@ class ConnectingFunction(nn.Module):
         elif cnf == 'OR':
             self.cnf = lambda x,y: x+y-(x*y)
         elif cnf == 'XOR':
-            #self.cnf = lambda x,y: x+y-(2*x*y)
-            self.cnf = lambda x,y: torch.where(z:=x+y>1.0,0.0,z)
+            self.cnf = xor_cnf
+        elif cnf == 'MXOR':
+            self.cnf = modulus_xor_cnf
+        elif cnf == 'WXOR':
+            self.cnf = where_xor_cnf
         else:
             raise NotImplementedError(f'{cnf} is a connecting function that has not been implemented.')
     def forward(self,x,y):
