47ce0346d79f3c8cc1373da0603d40f9f056ce02
alexandercantrell <alexanderjoelcantrell@gmail.com>
2023-05-21 21:17:51 -0500

    Modified config values

--- train/config.py
+++ train/config.py
@@ -41,7 +41,7 @@ _C.DATA.CACHE_MODE = 'part'
 # Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.
 _C.DATA.PIN_MEMORY = True
 # Number of data loading threads
-_C.DATA.NUM_WORKERS = 8
+_C.DATA.NUM_WORKERS = 16
 
 # -----------------------------------------------------------------------------
 # Model settings
