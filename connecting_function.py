8e78443a048da954a640811259290947dc09fd99
alexandercantrell <alexanderjoelcantrell@gmail.com>
2023-05-22 00:05:14 -0500

    Finalized connecting functions.

--- connecting_function.py
+++ connecting_function.py
@@ -5,20 +5,14 @@ def add_cnf(x,y):
     return x+y
 
 def and_cnf(x,y):
-    return x*y
-
-def where_and_cnf(x,y):
     return nn.functional.relu(x+y-1)
 
 def iand_cnf(x,y):
-    return x * (1. - y) 
-
-def where_iand_cnf(x,y):
     return nn.functional.relu(x-y)
 
 def or_cnf(x,y):
     z = x+y
-    return torch.where(z>0.0,1.0,z)
+    return torch.where(z>1.0,1.0,z)
 
 def xor_cnf(x,y):
     return (x+y)%2
@@ -30,12 +24,8 @@ class ConnectingFunction(nn.Module):
             self.cnf = lambda x,y: x+y
         elif cnf == 'AND':
             self.cnf = and_cnf
-        elif cnf == 'WAND':
-            self.cnf = where_and_cnf
         elif cnf == 'IAND':
             self.cnf = iand_cnf
-        elif cnf == 'WIAND':
-            self.cnf = where_iand_cnf
         elif cnf == 'OR':
             self.cnf = or_cnf
         elif cnf == 'XOR':
