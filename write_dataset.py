af511bb0b869eaf1634ae3d21cb6e0f9269ed1de
alexandercantrell <alexanderjoelcantrell@gmail.com>
2023-06-02 17:19:00 -0500

    Fixing bug where when trying to convert one split after a previous one the os.makedirs call threw an error due to pre-existing directories.

--- write_dataset.py
+++ write_dataset.py
@@ -34,7 +34,7 @@ def main(args):
         dataset = ImageFolder(root=os.path.join(args.data_path,args.split))
     if args.subset>0: 
         dataset = Subset(dataset,range(args.subset))
-    os.makedirs(args.write_path)
+    os.makedirs(args.write_path,exist_ok=True)
     path = os.path.join(args.write_path,f'{args.split}.ffcv')
     writer = DatasetWriter(path, {
         'image': RGBImageField(write_mode=args.write_mode,
