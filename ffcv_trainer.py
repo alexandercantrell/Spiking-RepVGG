84ae9d9f0afa466ba2a452fa93183a96897dd501
alexandercantrell <alexanderjoelcantrell@gmail.com>
2023-06-04 10:49:01 -0500

    Added num_classes to accuracy metric

--- ffcv_trainer.py
+++ ffcv_trainer.py
@@ -465,10 +465,11 @@ class ImageNetTrainer:
         return stats
 
     @param('logging.folder')
-    def initialize_logger(self, folder):
+    @param('model.num_classes')
+    def initialize_logger(self, folder, num_classes):
         self.val_meters = {
-            'top_1': torchmetrics.Accuracy(task='multiclass',compute_on_step=False).to(self.gpu),
-            'top_5': torchmetrics.Accuracy(task='multiclass',compute_on_step=False, top_k=5).to(self.gpu),
+            'top_1': torchmetrics.Accuracy(task='multiclass',num_classes=num_classes,compute_on_step=False).to(self.gpu),
+            'top_5': torchmetrics.Accuracy(task='multiclass',num_classes=num_classes,compute_on_step=False, top_k=5).to(self.gpu),
             'loss': MeanScalarMetric(compute_on_step=False).to(self.gpu)
         }
 
