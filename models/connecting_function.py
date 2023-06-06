a0e1d84e3009e63e4526188228351ff2ab270eb2
alexandercantrell <alexanderjoelcantrell@gmail.com>
2023-06-06 00:17:11 -0500

    Adding differentiable connecting functions

--- models/connecting_function.py
+++ models/connecting_function.py
@@ -1,7 +1,6 @@
 import torch
 from torch import nn
-from spikingjelly.activation_based import layer, functional
-import copy
+from spikingjelly.activation_based import layer
 
 def conv_bn(in_channels, out_channels, kernel_size, stride, padding, groups=1):
     result = nn.Sequential()
@@ -16,15 +15,27 @@ def add_cnf(x,y):
 def and_cnf(x,y):
     return nn.functional.relu(torch.subtract(torch.add(x,y),1))
 
+def dand_cnf(x,y):
+    return x * y
+
 def iand_cnf(x,y):
     return nn.functional.relu(torch.subtract(x,y))
+
+def diand_cnf(x,y):
+    return x * (1. - y)
 
 def or_cnf(x,y):
     z = torch.add(x,y)
     return torch.where(z>1.0,1.0,z)
 
+def dor_cnf(x,y):
+    return x+y-(x*y)
+
 def xor_cnf(x,y):
     return torch.remainder(torch.add(x,y),2)
+
+def dxor_cnf(x,y):
+    return x+y-(2*x*y)
 
 class ConnectingFunction(nn.Module):
     def __init__(self,cnf):
@@ -33,12 +44,20 @@ class ConnectingFunction(nn.Module):
             self.cnf = lambda x,y: x+y
         elif cnf == 'AND':
             self.cnf = and_cnf
+        elif cnf == 'DAND':
+            self.cnf = dand_cnf
         elif cnf == 'IAND':
             self.cnf = iand_cnf
+        elif cnf == 'DIAND':
+            self.cnf = diand_cnf
         elif cnf == 'OR':
             self.cnf = or_cnf
+        elif cnf == 'DOR':
+            self.cnf = dor_cnf
         elif cnf == 'XOR':
             self.cnf = xor_cnf
+        elif cnf == 'DXOR':
+            self.cnf = dxor_cnf
         else:
             raise NotImplementedError(f'{cnf} is a connecting function that has not been implemented.')
     def forward(self,x,y):
