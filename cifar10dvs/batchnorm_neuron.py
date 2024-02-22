c345345bd93bf58993ae82db6a624036960d2343
alexandercantrell <alexanderjoelcantrell@gmail.com>
2024-02-22 10:09:37 -0600

    added cupy backend to bnlif neuron and added syops hooks

--- cifar10dvs/batchnorm_neuron.py
+++ cifar10dvs/batchnorm_neuron.py
@@ -29,7 +29,7 @@ class BNPLIFNode(BaseNode):
         if self.step_mode == 's':
             return ('torch',)
         elif self.step_mode =='m':
-            return ('torch',)
+            return ('torch','cupy')
         else:
             raise ValueError(self.step_mode)
         
